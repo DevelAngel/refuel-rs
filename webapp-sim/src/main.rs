4f61da8a8a15e807400889d3f22523dcd20bc65d
Angelos Drossos <develangel@mail.drossos.de>
2023-05-11 21:55:01 +0200

    feat(sim): first component

--- webapp-sim/src/main.rs
+++ webapp-sim/src/main.rs
@@ -1,6 +1,9 @@
+mod app;
+
+use crate::app::{App, AppProps};
+
 use leptos::*;
 
-// Easy to use with Trunk (trunkrs.dev) or with a simple wasm-bindgen setup
 pub fn main() {
-    mount_to_body(|cx| view! { cx,  <p>"Hello Refuel User"</p> })
+    mount_to_body(|cx| view! { cx,  <App/> })
 }
