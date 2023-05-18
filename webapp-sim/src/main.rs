33e51b518770f70ec1557ddbb09e8be86922faab
Angelos Drossos <develangel@mail.drossos.de>
2023-05-18 08:26:16 +0200

    feat(sim): helloworld with axum

--- webapp-sim/src/main.rs
+++ webapp-sim/src/main.rs
@@ -1,9 +1,31 @@
-mod app;
+use std::net::SocketAddr;
 
-use crate::app::{App, AppProps};
+use axum::{response::Html, routing::get, Router};
 
-use leptos::*;
+use tracing_subscriber::EnvFilter;
 
-pub fn main() {
-    mount_to_body(|cx| view! { cx,  <App/> })
+use tracing::info;
+
+async fn home() -> Html<&'static str> {
+    Html("Hello world")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .with_writer(std::io::stderr)
+        .compact()
+        .init();
+
+    let app = Router::new()
+        .route("/", get(home));
+
+    let addr: SocketAddr = "127.0.0.1:8080".parse().expect("invalid socket address");
+    info!("listening on http://{}", addr.to_string());
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
 }
