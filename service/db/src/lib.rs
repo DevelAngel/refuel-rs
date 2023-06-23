758abf0ca0edf83ce1e892ff8ca935f45374f2fa
Angelos Drossos <develangel@mail.drossos.de>
2023-06-23 22:17:11 +0200

    feat(service/db): set pragma setting for connection handling

--- service/db/src/lib.rs
+++ service/db/src/lib.rs
@@ -13,10 +13,23 @@ use std::env;
 use diesel::prelude::*;
 
 pub fn establish_connection_sqlite() -> AnyConnection {
+    use diesel::connection::SimpleConnection;
+
     dotenv().ok();
 
+    let pragmas = concat!(
+        "PRAGMA journal_mode = WAL; ",
+        "PRAGMA synchronous = NORMAL; ",
+        "PRAGMA wal_autocheckpoint = 1000; ",
+        "PRAGMA wal_checkpoint(TRUNCATE); ",
+        "PRAGMA busy_timeout = 250; ",
+        "PRAGMA foreign_keys = ON; ",
+    );
+
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let conn = SqliteConnection::establish(&database_url)
+    let mut conn = SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+    conn.batch_execute(pragmas)
+        .unwrap_or_else(|_| panic!("Error setting connection pragmas"));
     AnyConnection::Sqlite(conn)
 }
