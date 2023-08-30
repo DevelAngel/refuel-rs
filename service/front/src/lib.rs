67d6a594712ae81cb7f3544cbed99f77ed3435cd
Angelos Drossos <develangel@mail.drossos.de>
2023-08-30 20:14:01 +0200

    feat: use tracing in front

--- service/front/src/lib.rs
+++ service/front/src/lib.rs
@@ -7,9 +7,10 @@ use leptos::*;
 #[wasm_bindgen]
 pub fn hydrate() {
     console_error_panic_hook::set_once();
-    _ = console_log::init_with_level(log::Level::Debug);
 
-    log!("hydrate mode - hydrating");
+    tracing_wasm::set_as_global_default();
+
+    tracing::info!("hydrate mode - hydrating");
 
     leptos::mount_to_body(|cx| {
         view! { cx,  <App/> }
